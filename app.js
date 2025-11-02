ec6b3beab35b7c0763aaea113f088eebed08668a
Paulo Salvador <pauloualopsalvador@gmail.com>
2025-11-02 23:40:27 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function addToCart(item) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(item);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Added to cart!");
+}
---
+++ cart.js
@@ -0,0 +1,14 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+let cartOutput = document.getElementById("cart-output");
+cartOutput.innerHTML = "";
+
+if (cart.length === 0) {
+  cartOutput.innerHTML = "<p>Cart is empty.</p>";
+} else {
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = `${item.name} - ₱${item.price}`;
+    cartOutput.appendChild(li);
+  });
+}
